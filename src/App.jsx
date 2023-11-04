500cca616884fd5e37809a84ac76b9bf934824d5
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-11-04 10:29:34 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import useDarkMode from 'use-dark-mode';
 import AppContext from './AppContext';
 import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
 import { lightTheme, darkTheme } from './theme/themes';
+
 
 function App() {
   window.matchMedia = null;
@@ -18,9 +19,10 @@ function App() {
       <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
         <GlobalStyles />
         <div className="App">
-          <BrowserRouter>
+          <HashRouter>
+            {/* <BrowserRouter basename="/dev-portfolio"> */}
             <MainApp />
-          </BrowserRouter>
+          </HashRouter>
         </div>
       </ThemeProvider>
     </AppContext.Provider>
